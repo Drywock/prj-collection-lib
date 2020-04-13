06fc14abb19e9a44117cfdad4765eb3d8c836213
Thomas LINTANF <thomas.lintanf@efrei.net>
2020-04-13 16:31:47 +0200

    Switch indent to tabs

--- Drwk/Iterable.h
+++ Drwk/Iterable.h
@@ -7,36 +7,36 @@ template <typename T>
 class Iterator
 {
 public:
-    // Constructor
-    Iterator(T *value) : m_value(value) {}
+	// Constructor
+	Iterator(T *value) : m_value(value) {}
 
-    // Methods
-    virtual T &value(void){return *m_value};
-    virtual void next() = 0;
+	// Methods
+	virtual T &value(void){return *m_value};
+	virtual void next() = 0;
 
-    // Operators
-    friend bool operator!=(const Iterator<T> &a, const Iterator<T> &b);
-    friend bool operator==(const Iterator<T> &a, const Iterator<T> &b);
+	// Operators
+	friend bool operator!=(const Iterator<T> &a, const Iterator<T> &b);
+	friend bool operator==(const Iterator<T> &a, const Iterator<T> &b);
 
-    void operator++();
-    void operator+=(const int &n);
-    friend Iterator<T> operator+(Iterator<T> a, const int &n);
+	void operator++();
+	void operator+=(const int &n);
+	friend Iterator<T> operator+(Iterator<T> a, const int &n);
 
 private:
-    T *m_value;
+	T *m_value;
 };
 
 template <typename T>
 class Iterable
 {
 public:
-    Iterator<T> begin();
-    Iterator<T> end();
-    void forEach(void (*func)(T &));
+	Iterator<T> begin();
+	Iterator<T> end();
+	void forEach(void (*func)(T &));
 
 private:
-    Iterator<T> m_begin;
-    Iterator<T> m_end;
+	Iterator<T> m_begin;
+	Iterator<T> m_end;
 };
 
 // Iterrator methods definitions
@@ -44,32 +44,32 @@ private:
 template <typename T>
 bool operator==(const Iterator<T> &a, const Iterator<T> &b)
 {
-    return a.m_value == b.m_value;
+	return a.m_value == b.m_value;
 }
 
 template <typename T>
 bool operator!=(const Iterator<T> &a, const Iterator<T> &b)
 {
-    return !(a == other);
+	return !(a == other);
 }
 
 template <typename T>
 void Iterator<T>::operator++()
 {
-    next();
+	next();
 }
 
 template <typename T>
 void Iterator<T>::operator+=(const int &n)
 {
-    for (int i = 0; i < n; i++)
-        *this ++;
+	for (int i = 0; i < n; i++)
+		*this ++;
 }
 
 template <typename T>
 Iterator<T> operator+(Iterator<T> a, const int &n)
 {
-    return a += n;
+	return a += n;
 }
 
 // Iterable methods definitions
@@ -77,10 +77,10 @@ Iterator<T> operator+(Iterator<T> a, const int &n)
 template <typename T>
 void Iterable<T>::forEach(void (*func)(T &))
 {
-    Iterator<T> iterator = m_begin;
-    while (iterator != m_end)
-    {
-        func(iterator.value());
-    }
+	Iterator<T> iterator = m_begin;
+	while (iterator != m_end)
+	{
+		func(iterator.value());
+	}
 }
 } // namespace Drwk
--- Drwk/vector.h
+++ Drwk/vector.h
@@ -9,42 +9,42 @@ template <typename T>
 class Vector : public Collection<T>, public Itterable<T>
 {
 public:
-    Vector() : m_data(nullptr), m_size(0) {}
-    ~Vector();
+	Vector() : m_data(nullptr), m_size(0) {}
+	~Vector();
 
-    void push(T);
-    T pop();
-    int size() { return m_size; }
+	void push(T);
+	T pop();
+	int size() { return m_size; }
 
 private:
-    T *m_data;
-    int m_size;
+	T *m_data;
+	int m_size;
 };
 
 // Iterator
 template <typename T>
 class Vectorterator : Iterator<T>
 {
-    virtual next() override { data++; }
+	virtual next() override { data++; }
 };
 
 // Definitions
 template <typename T>
 Vector<T>::~Vector<T>()
 {
-    delete[] m_data;
+	delete[] m_data;
 }
 
 template <typename T>
 void Vector<T>::push(T value)
 {
-    if (m_size == 0)
-    {
-        m_data = new T[1];
-        m_data[0] = T;
-    }
-    else
-    {
-    }
+	if (m_size == 0)
+	{
+		m_data = new T[1];
+		m_data[0] = T;
+	}
+	else
+	{
+	}
 }
 } // namespace Drwk
